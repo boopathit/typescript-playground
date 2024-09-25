5ea2c506abcad4994adc570eaef4c531407896e2
Boopathi T <getboopathi.t@gmail.com>
2024-09-25 13:25:57 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 
+function LoggerDecorator(logMessage:string) {
+     function Logger(target:Function) {
+        console.log(logMessage+" : " + target);
+    }
+    return Logger;
+}
+function LoggerNumberField(target:any) {
+    console.log("Prop Decorator..." + target);
+}
+
+@LoggerDecorator("from User")
+class User {
+    @LoggerNumberField
+    name:string = "Boo";
+    age:number = 31
+    constructor(){
+        console.log("User class construtor called...")
+    }
+}
+const u = new User();
